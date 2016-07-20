7e62052f0f6ab3792291aab67ba8863c72badb82
Shivang Kar <kar.shivang007@gmail.com>
2016-07-20 10:16:37 +0530

    Add files via upload

--- Cards.jsx
+++ Cards.jsx
@@ -9,11 +9,11 @@ class Cards extends React.Component{
 			display: 'none'
 		}
 		return(
-				
+				<div className= "col-xs-12 col-sm-6 col-md-3 col-lg-3">
 				<div id = {this.props.data.id+'div'} className = {this.props.data.className} onClick = {this.props.onClick}>
 					<div id = {this.props.data.id} className = 'hiddenText' style = {hiddenText}>{this.props.data.cardValue}</div>
 				</div>
-
+				</div>
 			);
 	}
 
